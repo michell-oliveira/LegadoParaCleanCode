ceca85a8b5012783eedae6cff9d5560a09266f83
Luis Miranda <luisgustavodemiranda@gmail.com>
2025-07-07 11:11:30 -0300

    Legado para Clean Code
    Alterações na estrutura do projeto

--- Legado.dpr
+++ Legado.dpr
@@ -2,13 +2,18 @@ program Legado;
 
 uses
   Vcl.Forms,
-  UnitPrincipal in 'UnitPrincipal.pas' {Form1};
+  View.Pessoa in 'View\View.Pessoa.pas' {FrmPessoa},
+  DAO.Pessoa.SQLite in 'DAO\DAO.Pessoa.SQLite.pas',
+  Controller.Pessoa in 'Controller\Controller.Pessoa.pas',
+  Model.Pessoa in 'Model\Model.Pessoa.pas',
+  DAO.Pessoa.CSV in 'DAO\DAO.Pessoa.CSV.pas',
+  Conexao.Tipo in 'Tipos\Conexao.Tipo.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TFrmPessoa, FrmPessoa);
   Application.Run;
 end.
